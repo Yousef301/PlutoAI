aa3acc4bb1d1df0b0279e63bdc6a8640231b4d40
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-12-11 16:38:15 +0200

    [3] Implement password hashing service

---
+++ Pluto.Application/Services/Implementations/BCryptPasswordService.cs
@@ -0,0 +1,18 @@
+﻿using Pluto.Application.Services.Interfaces;
+
+
+namespace Pluto.Application.Services.Implementations;
+
+public class BCryptPasswordService : IPasswordService
+{
+    public string HashPassword(string plainPassword)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(plainPassword);
+    }
+
+    public bool ValidatePassword(string plainPassword,
+        string hashedPassword)
+    {
+        return BCrypt.Net.BCrypt.Verify(plainPassword, hashedPassword);
+    }
+}
---
+++ Pluto.Application/Services/Interfaces/IPasswordService.cs
@@ -0,0 +1,7 @@
+﻿namespace Pluto.Application.Services.Interfaces;
+
+public interface IPasswordService
+{
+    string HashPassword(string plainPassword);
+    bool ValidatePassword(string plainPassword, string hashedPassword);
+}
