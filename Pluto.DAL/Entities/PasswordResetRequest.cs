37e81d0cc42d2da41b2040e80a52b98ec8990ae0
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-12-14 18:46:03 +0200

    [18] Add PasswordReset table and migrate it

---
+++ Pluto.DAL/Entities/PasswordResetRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Pluto.DAL.Entities;
+
+public class PasswordResetRequest : BaseEntity
+{
+    public int UserId { get; set; }
+    public Guid Token { get; set; }
+    public long? ExpiryDate { get; set; }
+
+    public User User { get; set; }
+}
---
+++ Pluto.DAL/Migrations/20241214164048_AddPasswordResetTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pluto.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordResetTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PasswordResetRequest",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Token = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExpiryDate = table.Column<long>(type: "bigint", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasswordResetRequest", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PasswordResetRequest_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PasswordResetRequest_UserId",
+                table: "PasswordResetRequest",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PasswordResetRequest");
+        }
+    }
+}
