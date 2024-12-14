d280457bd336dd13fcdebb36eed523a6e7dcc09f
Yousef Shamasneh <shamasnehyousef173@gmail.com>
2024-12-14 12:13:26 +0200

    [16] Implement sending email via SMTP service

---
+++ Pluto.Application/Services/SharedServices/Interfaces/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿using Pluto.Application.DTOs.Auth;
+
+namespace Pluto.Application.Services.SharedServices.Interfaces;
+
+public interface IEmailService
+{
+    Task SendEmailAsync(string email, string subject, EmailConfirmationBody body);
+}
